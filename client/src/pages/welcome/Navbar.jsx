bacb6425169720f35898d5049a16ca19570dfc0f
eya-harbaoui <102240094+eya-harbaoui@users.noreply.github.com>
2023-04-16 15:23:45 +0100

    navbar en cours

--- client/src/pages/welcome/Navbar.jsx
+++ client/src/pages/welcome/Navbar.jsx
@@ -1,13 +1,24 @@
 import "./NavbarStyles.css";
 import React from "react";
+import { useState } from "react";
 import { FaHome } from "react-icons/fa";
 import { BsFillExclamationCircleFill } from "react-icons/bs";
 import { BiUserPin } from "react-icons/bi";
+import {AiOutlineBars} from "react-icons/ai";
+import {GrClose} from "react-icons/gr";
 import { MenuLinks } from "./MenuLinks";
 function Navbar() {
+  const [toggle, setToggle] = useState("not toggled");
+
   return (
     <nav className="NavbarItems">
       <h1 className="navbar-logo">Web Scraping Automation</h1>
+      <div className="menu-icons">
+        {toggle== "toggled" && <AiOutlineBars></AiOutlineBars>}
+        {toggle== "not toggled" && <GrClose></GrClose>}
+        <GrClose></GrClose>
+      </div>
+
       <ul className="nav-menu">
         {MenuLinks.map((item, index) => {
           //display icons according to their names in menuLinks list
@@ -23,7 +34,7 @@ function Navbar() {
           return (
             <li key={index}>
               <a href={item.url} className={item.cName}>
-                {IconComponent && <IconComponent/>}
+                {IconComponent && <IconComponent />}
                 {item.title}
               </a>
             </li>
