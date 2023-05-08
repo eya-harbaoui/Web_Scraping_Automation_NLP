e15a13f20ef8102ce13d77fbd1f9f19a1f21a2d7
eya-harbaoui <102240094+eya-harbaoui@users.noreply.github.com>
2023-05-08 04:28:18 +0100

    main page avancement

--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -1,42 +1,125 @@
-import React, {useState} from "react";
-import Navbar from '../welcome/Navbar'
-import { Button, Popover } from "antd";
+import React, { useState, useCallback, useMemo, useRef } from "react";
+import Navbar from "../welcome/Navbar";
+import { Button, Popover, Result } from "antd";
 import axios from "axios"; // for making HTTP requests
 import cheerio from "cheerio"; // for web scraping
-import {Stitch, AnonymousCredential} from "mongodb-stitch-browser-sdk"; // for connecting to MongoDB
-import "./index.css"
-import { Card } from "antd";
-import { Table } from "semantic-ui-react";
+import { Stitch, AnonymousCredential } from "mongodb-stitch-browser-sdk"; // for connecting to MongoDB
+import "./index.css";
+import { Space, Table, Tag } from "antd";
+import { BsRobot } from "react-icons/bs";
 import { Container, Header, List } from "semantic-ui-react";
 function Main() {
-  // State variables for storing the input URL and extracted labels
-  const [url, setUrl] = useState("");
-  const [title, setTitle] = useState("Here is the article title");
-  const [body, setBody] = useState("Here is the article content");
-  const [author, setAuthor] = useState("Here is the article author name");
-  const [source, setSource] = useState("Here is the article source");
-  const [topic, setTopic] = useState("Here is the article topic");
-  const [date, setDate] = useState("Here is the article date");
-  //
-  const SendLink = async () => {
-      try {
-        const resp = await axios.post("http://localhost:8000/predict", url);
-        if (resp.data.error) {
-          alert("error")
-          
-        } else {
-          console.log(resp.data);
-        }
-      } catch (error) {
-        console.log(error);
+  // State variables for storing the input link and extracted labels
+  const [link, setLink] = useState("");
+  const [nodes, setNodes] = useState([]);
+  const [xpaths, setXpaths] = useState([]);
+  const [labels, setLabels] = useState([]);
+  const [result, setResult] = useState([]);
+const columns = [
+  {
+    title: "Node",
+    dataIndex: "node",
+    key: "node",
+    width: 500,
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Xpath",
+    dataIndex: "xpath",
+    key: "xpath",
+  },
+  {
+    title: "Predictions",
+    key: "label",
+    dataIndex: "label",
+    render: (label) => {
+      let color, fontSize;
+      switch (label) {
+        case "title":
+          color = "blue";
+          fontSize = "16px";
+          break;
+        case "body":
+          color = "green";
+          fontSize = "16px";
+          break;
+        case "date":
+          color = "red";
+          fontSize = "16px";
+          break;
+        case "source":
+          color = "green";
+          fontSize = "16px";
+          break;
+        case "topic":
+          color = "green";
+          fontSize = "16px";
+          break;
+        case "author":
+          color = "green";
+          fontSize = "16px";
+          break;
+        default:
+          color = "geekblue";
+          fontSize = "16px";
       }
-    };
+      return (
+        <Tag color={color} style={{ fontSize }}>
+          {label}
+        </Tag>
+      );
+    },
+  },
+];
+
+  const SendLink = async () => {
+    try {
+      const resp = await axios.post("http://localhost:8000/predict", {
+        link: link,
+      });
+      if (resp.data.error) {
+        alert("error");
+      } else {
+        console.log(resp.data.result);
+        console.log(result,"rani resulttttttttttttt")
+        setNodes(resp.data.result.Node);
+        console.log("nodeeees", nodes);
+        setXpaths(resp.data.result.Xpath);
+        setLabels(resp.data.result.Predicted);
+        let outputList = [];
+
+        for (let i = 0; i < nodes.length; i++) {
+          let outputDict = {
+            node: nodes[i],
+            xpath: xpaths[i],
+            label: labels[i],
+          };
+          if(outputDict.label!="other"){
+            outputList.push(outputDict);
+          }
+
+          
+          
+          
+          setResult(outputList);
+          
+        }
+
+        console.log(result,"ena result lekhraaaaaaaaaaa");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+
   // Function to handle form submit and trigger web scraping
   const handleSubmit = async (e) => {
     e.preventDefault(); // prevent page from reloading
 
     // Make HTTP request to fetch webpage content
-    const response = await axios.get(url);
+    const response = await axios.get(link);
 
     // Use Cheerio to parse HTML and extract labels
     //const $ = cheerio.load(response.data);
@@ -47,13 +130,13 @@ function Main() {
     //setTopic($('meta[property="article:tag"]').attr("content"));
 
     // Save extracted labels to MongoDB
-    const client = Stitch.initializeDefaultAppClient("<YOUR-APP-ID>");
-    await client.auth.loginWithCredential(new AnonymousCredential());
-    const database = client
-      .getServiceClient(Stitch.RemoteMongoClient.factory, "<YOUR-DB-NAME>")
-      .db("<YOUR-COLLECTION-NAME>");
-    const result = await database.insertOne({
-      url,
+    //const client = Stitch.initializeDefaultAppClient("<YOUR-APP-ID>");
+    //await client.auth.loginWithCredential(new AnonymousCredential());
+    //const database = client
+    //.getServiceClient(Stitch.RemoteMongoClient.factory, "<YOUR-DB-NAME>")
+    //.db("<YOUR-COLLECTION-NAME>");
+    /*const result = await database.insertOne({
+      link,
       title,
       body,
       author,
@@ -61,46 +144,60 @@ function Main() {
       topic,
       timestamp: new Date(),
     });
-    console.log(result.insertedId);
+    console.log(result.insertedId);*/
   };
-   const [open, setOpen] = useState(false);
-   const hide = () => {
-     setOpen(false);
-   };
-   const handleOpenChange = (newOpen) => {
-     setOpen(newOpen);
-   };
+  const [open, setOpen] = useState(false);
+  const hide = () => {
+    setOpen(false);
+  };
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
+
+  const CleanChat = ()=>{
+    setXpaths([]);
+    setNodes([]);
+  setLabels([]);  }
+  // Map over the data to generate table rows
 
   return (
     <>
       <div className="App">
         <aside className="sidemenu">
-          <div className="side-menu-button">
+          <div className="side-menu-button" onClick={CleanChat}>
             <span>+</span>
             Clear Research
           </div>
+          <ul>
+            <li>{link}</li>
+          </ul>
+          
         </aside>
         <section className="chatbox">
           <div className="result">
-            <h1>Web Scraping</h1>
+            <h1>Web Scraping Automation</h1>
+            <BsRobot className="robot-icon"></BsRobot>
+
             <form onSubmit={handleSubmit}>
               <Popover
                 placement="topRight"
                 content={<a onClick={hide}>Close</a>}
-                title="Enter the finance article url here "
+                title="Enter the finance article link here "
                 trigger="click"
                 open={open}
                 onOpenChange={handleOpenChange}
               >
                 <input
                   type="text"
-                  value={url}
-                  onChange={(e) => setUrl(e.target.value)}
+                  value={link}
+                  onChange={(e) => setLink(e.target.value)}
                   placeholder=""
                 />
               </Popover>
 
-              <button type="submit" onClick={SendLink}>Scrape</button>
+              <button type="submit" onClick={SendLink}>
+                Scrape
+              </button>
             </form>
 
             <div>
@@ -108,9 +205,9 @@ function Main() {
             </div>
           </div>
           <div className="chat-log">
-            <div className="chat-message"></div>
-            <div className="avatar"></div>
-            <div className="message">Result</div>
+            <div className="message">
+              <Table columns={columns} dataSource={result} />
+            </div>
           </div>
         </section>
       </div>
